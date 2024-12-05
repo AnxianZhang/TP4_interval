c6d061d6a5f44d758b63f62b65774af50f258490
AnxianZhang <gadanxianzhang@gmail.com>
2024-12-05 18:00:10 +0100

    prototype added

--- Node.h
+++ Node.h
@@ -15,4 +15,22 @@ typedef struct Node {
 
 typedef Node *Tree;
 
+Node *createNode(unsigned int id, char *description, Interval *interval);
+
+void addReservation(Tree *tree, unsigned int id, Interval *interval, char *description);
+
+Node *searchReservation(const Tree *tree, Interval *interval, unsigned int id);
+
+void deleteReservation(Tree *tree, Interval *interval, unsigned int id);
+
+void updateReservation(Tree *tree, Interval *current, Interval *newInterval, unsigned int id);
+
+void showTree(const Tree *tree);
+
+void showCompany(const Tree *tree, unsigned int id);
+
+void showPeriod(const Tree *tree, const Interval *period);
+
+void deleteAllFree(const Tree *tree);
+
 #endif //NODE_H
