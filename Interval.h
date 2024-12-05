c9d06da4be2a48d0755f5a98eebd66ca5361182d
AnxianZhang <gadanxianzhang@gmail.com>
2024-12-05 17:28:14 +0100

    inital commit

---
+++ Interval.h
@@ -0,0 +1,8 @@
+//
+// Created by gadan on 12/5/2024.
+//
+
+#ifndef INTERVAL_H
+#define INTERVAL_H
+
+#endif //INTERVAL_H
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+//
+// Created by gadan on 12/5/2024.
+//
+int main() {
+    printf("toto");
+    return 0;
+}
