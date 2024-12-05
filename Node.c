866203cd86bf3d04b37941dcb32bec617e44fc81
AnxianZhang <gadanxianzhang@gmail.com>
2024-12-05 19:14:46 +0100

    create node function added

--- Node.c
+++ Node.c
@@ -3,3 +3,23 @@
 //
 
 #include "Node.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+Node *createNode(unsigned int id, char *description, Interval *interval) {
+    if (!interval) return NULL;
+
+    Node *n = malloc(sizeof(Node));
+    if (!n) return NULL;
+
+    n->id = id;
+    n->description = malloc(sizeof(char) * strlen(description));
+    strcpy(n->description, description);
+    n->interval = interval;
+    n->left = NULL;
+    n->right = NULL;
+
+    return n;
+}
--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "Interval.h"
+#include "Node.h"
 //
 // Created by gadan on 12/5/2024.
 //
+
+void test() {
+    Interval *i = malloc(sizeof(Interval));
+    i->start = 0110;
+    i->end = 1025;
+    Node *n = createNode(1, "J'aime les pommes au cafe", i);
+}
+
 int main() {
-    printf("toto");
+    test(); // fonction utilsier pour les testes en dure
+
     return 0;
 }
