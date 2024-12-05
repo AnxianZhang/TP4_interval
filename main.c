185140967c30082cafad83cdbd34c34f7611d3fd
AnxianZhang <gadanxianzhang@gmail.com>
2024-12-05 21:08:39 +0100

    show tree added

--- main.c
+++ main.c
@@ -9,9 +9,27 @@
 
 void test() {
     Interval *i = malloc(sizeof(Interval));
-    i->start = 0110;
-    i->end = 1025;
-    Node *n = createNode(1, "J'aime les pommes au cafe", i);
+    i->start = 110;
+    i->end = 225;
+
+    Interval *i2 = malloc(sizeof(Interval));
+    i2->start = 225;
+    i2->end = 402;
+
+    Interval *i3 = malloc(sizeof(Interval));
+    i3->start = 613;
+    i3->end = 1025;
+
+    Interval *i4 = malloc(sizeof(Interval));
+    i4->start = 101;
+    i4->end = 107;
+
+    Tree tree = createNode(1, "J'aime les pommes au cafe", i);
+    tree->right = createNode(2, "pomme ?", i2);
+    tree->right->right = createNode(3, "cafe ?", i3);
+    tree->left = createNode(4, "1e fruit ?", i4);
+
+    showTree(tree);
 }
 
 int main() {
